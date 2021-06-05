5b662998b0da182416831de40fa8cea46e4b8052
GorohovDmitriy <dmitriy2893@gmail.com>
2021-06-05 09:56:01 +0300

    создали приложение, установили зависимости (axios, node-sass, bootstrap, react-router-dom, react-transition-group)

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+	<div className='jumbotron'>
+		<div className='container'>
+			<h1 className='display-4'>Todo Aplication</h1>
+			<p className='lead'>
+				Version 1.0.42
+			</p>
+		</div>
+	</div>
+)
